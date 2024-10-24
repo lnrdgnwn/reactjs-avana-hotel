547f6fe60f2b8e4520cbdb004cbc38d618505331
lnrdgnwn <leonardogunawan15@gmail.com>
2024-10-24 10:05:08 +0700

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,31 +3,33 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      primary: 'Gilda Display',
-      secondary: 'Barlow',
-      tertiary: 'Barlow Condensed',
+      primary: "Gilda Display",
+      secondary: "Barlow",
+      tertiary: "Barlow Condensed",
     },
     container: {
       padding: {
-        DEFAULT: '15px',
+        DEFAULT: "15px",
       },
     },
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '960px',
-      xl: '1140px',
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1140px",
     },
     extend: {
       colors: {
-        primary: '#0a0a0a',
+        primary: "#0a0a0a",
         accent: {
-          DEFAULT: '#a37d4c',
-          hover: '#967142',
+          DEFAULT: "#a37d4c",
+          hover: "#967142",
         },
       },
       backgroundImage: {
         room: "url('./assets/img/room.jpg')",
+        restaurant: "url('./assets/img/restaurant.jpg')",
+        spa: "url('./assets/img/spa.jpg')",
       },
     },
   },
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-avana-hotel/',
+  base: "/reactjs-avana-hotel/",
   plugins: [react()],
-})
+});
