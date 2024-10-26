73dc917ffe90ae0c146edce3fbbcd4fb37175695
lnrdgnwn <leonardogunawan15@gmail.com>
2024-10-26 19:03:21 +0700

    add(restaurant): add restaurant pages

---
+++ src/pages/Restaurant.jsx
@@ -0,0 +1,113 @@
+import RestaurantBackground from "../assets/img/restaurant.jpg";
+import { FaCheck, FaClock } from "react-icons/fa";
+import { FaPhoneVolume } from "react-icons/fa6";
+import { MdSmokeFree, MdMenu } from "react-icons/md";
+
+function Restaurant() {
+  return (
+    <section>
+      <div className="flex flex-col relative justify-center items-center bg-restaurant bg-cover bg-center h-[560px]">
+        <div className="absolute w-full h-full bg-black/70"></div>
+        <h1 className="text-6xl font-primary mt-20 text-white z-20">
+          Avana Restaurant
+        </h1>
+        <p className="text-white text-3xl z-20 mt-4 mb-6 font-tertiary">
+          Serving Best Foods for you!
+        </p>
+        <button className="btn-primary z-20 text-white font-tertiary uppercase tracking-[3px] btn-lg px-6 py-2 ">
+          Reserve Now!
+        </button>
+      </div>
+      <div className="container mx-auto py-24">
+        <div className="flex flex-col lg:flex-row h-full">
+          <div className="w-full h-full lg:w-[60%] px-6">
+            <img
+              className="hover:scale-105 transition-all duration-300 w-full"
+              src={RestaurantBackground}
+              alt=" "
+            />
+            <p className="mb-8"></p>
+            <div className="mt-12">
+              <h3 className="h3">Avana Restaurant</h3>
+              <p className="mb-12">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae
+                earum veritatis ipsa accusantium animi, quibusdam cupiditate,
+                iste expedita odio doloribus quae atque quos nobis totam
+                deleniti alias incidunt, quaerat debitis placeat possimus? A
+                architecto hic laboriosam nihil, similique quia quaerat.
+              </p>
+            </div>
+          </div>
+
+          <div className="w-full h-full lg:w-[40%] px-6">
+            <div className="flex flex-col space-y-4 mb-4">
+              <div>
+                <h3 className="h3">More Information</h3>
+                <p className="mb-6">
+                  Brunch Asian Fusion Buffet Every Saturday, Sunday, and Public
+                  Holiday @ IDR 498,000 net/pax Enjoy a variety of Asian
+                  delicacies, from sushi and ramen to grilled BBQ. Starting From
+                  10:00 AM – 2:30 PM
+                </p>
+                <ul className="flex flex-col gap-y-4">
+                  <li className="flex items-center gap-x-4">
+                    <FaPhoneVolume className="text-accent" />
+                    (+6119) 3820 5555 Ext. 7330
+                  </li>
+                  <li className="flex items-center gap-x-4">
+                    <div className="flex self-start py-1 restaurant-icon">
+                      <FaClock className="text-accent" />
+                    </div>
+                    <div className="flex flex-col">
+                      <div className="pb-2">
+                        <strong>Senin - Jumat</strong>
+                      </div>
+                      <div className="pb-3">
+                        <p>Lunch : 11.30 - 14.30</p>
+                        <p>Dinner : 18.00 - 22.30</p>
+                      </div>
+                      <div className="pb-2">
+                        <strong>
+                          Sabtu, Minggu, dan Hari Libur Nasional :{" "}
+                        </strong>
+                      </div>
+                      <div className="pb-2">
+                        <p>Lunch : 09.30 - 15.00</p>
+                        <p>Dinner : 18.00 - 22.30</p>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="flex items-center gap-x-4">
+                    <FaCheck className="text-accent" />
+                    No Pets
+                  </li>
+                  <li className="flex items-center gap-x-4">
+                    <MdSmokeFree className="text-accent" />
+                    No Smoking
+                  </li>
+                </ul>
+                <button className="btn-primary uppercase btn-sm px-6 mt-5 mr-3 tracking-[1px] text-white ">
+                  Reserve Now
+                </button>
+                <button className="btn-primary uppercase btn-sm px-6 mt-3 mr-3 tracking-[1px] text-white ">
+                  <div className="flex flex-row items-center justify-center gap-x-2">
+                    <MdMenu />
+                    <div>Menu</div>
+                  </div>
+                </button>
+                <button className="btn-primary uppercase btn-sm px-6 mt-3 mr-3 tracking-[1px] text-white ">
+                  <div className="flex flex-row items-center justify-center gap-x-2">
+                    <FaPhoneVolume />
+                    <div>Call</div>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Restaurant;
